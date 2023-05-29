b2585f9dd05bfb130adfbc064a50413f83b7b2b5
jaumpedro214 <joaopedrodasilvalima@gmail.com>
2023-05-28 22:42:47 -0300

    FIX: Dowload propostas now probably works correctly

--- get_propostas_camara.py
+++ get_propostas_camara.py
@@ -5,8 +5,8 @@ import json
 import logging
 
 BASE_URL = "https://dadosabertos.camara.leg.br/api/v2"
-N_ITENS_PER_PAGE = 50
-START_YEAR = 2003
+N_ITENS_PER_PAGE = 100
+MIN_YEAR = 1990
 
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(
@@ -20,6 +20,7 @@ if __name__ == "__main__":
         "pagina": 1,
         "ordem": "DESC",
         "ordenarPor": "ano",
+        "ano": 2023,
         "siglaTipo": "PL",
     }
     proposicoes = []
@@ -31,38 +32,35 @@ if __name__ == "__main__":
     stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 
-    logger.addHandler(stream_handler)
+    #logger.addHandler(stream_handler)
     logger.info("Iniciando script - get_propostas_camara.py")
     logger.info("BASIC PARAMS: " + json.dumps(params))
 
     while in_keep_querying_prepositions:
-        response = requests.get(f"{BASE_URL}/proposicoes", params=params)
 
-        logger.info(f'Fazendo requisição para a página {params["pagina"]}')
+        logger.info(f"Fazendo requisição para o ano de {params['ano']}")
 
-        if response.status_code == 200:
-            response_json = response.json()
-            data = response_json["dados"]
+        params["pagina"] = 1
+        while True:
+            logger.info(f'Fazendo requisição para a página {params["pagina"]}')
+            response = requests.get(f"{BASE_URL}/proposicoes", params=params)
 
-            if not data:
-                logger.info("Não há mais proposições")
-                break
-
-            for proposicao in data:
-                if proposicao["ano"] < START_YEAR:
-                    logger.info(
-                        f'Proposição {proposicao["id"]} é de {proposicao["ano"]}. Parando de fazer requisições'
-                    )
-                    in_keep_querying_prepositions = False
+            if response.status_code == 200:
+                response_json = response.json()
+                data = response_json["dados"]
+                if not data:
+                    logger.info(f"Não há mais proposições para o ano de {params['ano']}")
                     break
+                proposicoes = proposicoes + data
+            else:
+                logger.error(f"Erro na requisição. Status code: {response.status_code}")
+            params["pagina"] += 1
 
-                proposicoes.append(proposicao)
+        params["ano"] -= 1
 
-        else:
-            logger.error(f"Erro na requisição. Status code: {response.status_code}")
-            break
+        if params["ano"] == MIN_YEAR:
+            in_keep_querying_prepositions = False
 
-        params["pagina"] += 1
         time.sleep(0.5)
 
     with open(f"./data/proposicoes.json", 'w') as file:
